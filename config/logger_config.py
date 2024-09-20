6cdfad1d6a9f418bc79b5c769f57684e9cc49ede
Akhat <akhatsuleimenov@gmail.com>
2024-09-20 18:50:45 +0500

    fix: logging to stdout

--- config/logger_config.py
+++ config/logger_config.py
@@ -1,26 +1,20 @@
 import logging
-import os
-from logging.handlers import RotatingFileHandler
+import sys
 
 
 def setup_logging():
-    # Ensure the logs directory exists
-    log_dir = os.path.join(os.path.dirname(__file__), "..", "logs")
-    os.makedirs(log_dir, exist_ok=True)
-
-    # Path for the log file
-    log_file_path = os.path.join(log_dir, "telegram_bot.log")
-
+    # Configure logger
     logger = logging.getLogger("TelegramBotLogger")
     logger.setLevel(logging.INFO)
 
-    # Configure file handler
-    handler = RotatingFileHandler(log_file_path, maxBytes=5000000, backupCount=5)
+    # Configure stream handler to output logs to stdout
+    handler = logging.StreamHandler(sys.stdout)
     formatter = logging.Formatter(
         "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     )
     handler.setFormatter(formatter)
 
+    # Attach the handler to the logger
     if not logger.handlers:
         logger.addHandler(handler)
 
